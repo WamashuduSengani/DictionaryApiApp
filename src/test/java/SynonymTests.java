c48249b463b567e8086b73230d2509ce7857c0cc
Wamashudu Sengani <wamashudusengani2@gmail.com>
2022-10-08 18:55:27 +0200

    App complete, 1 test failing

---
+++ src/test/java/SynonymTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Synonyms")
+public class SynonymTests extends WebTestRunner {
+
+    private static WebSession session;
+
+    @BeforeEach
+    public void setupSession() {
+        session = new WebSession(this);
+    }
+
+    @Test
+    @DisplayName("lookup a word")
+    void lookupHasEverything() {
+        session.open(new MainPage(SynonymTests.this))
+                .shouldBeOnMainPage()
+                .clickOnSynonymsMenu()
+                .lookupWord("greed")
+                .shouldHaveWord("greed")
+                .shouldHaveRelatedWords();
+    }
+}
